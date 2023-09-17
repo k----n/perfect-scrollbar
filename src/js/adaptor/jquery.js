5ad43389a5f0412d3e388baf03c8f60ec5346726
k----n <kalvin.eng@ualberta.ca>
2023-09-16 21:31:03 -0600

    Add passive event listeners

--- src/js/adaptor/jquery.js
+++ src/js/adaptor/jquery.js
@@ -4,6 +4,18 @@ var ps = require('../main');
 var psInstances = require('../plugin/instances');
 
 function mountJQuery(jQuery) {
+  // Passive event listeners
+  jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+  };
+  jQuery.event.special.touchmove = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+  };
+
   jQuery.fn.perfectScrollbar = function (settingOrCommand) {
     return this.each(function () {
       if (typeof settingOrCommand === 'object' ||
